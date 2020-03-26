3c295345bcf7e7b9dd7a3824821204db19ca8acf
KasiaZd <kasiadzb@onet.eu>
2020-03-26 20:45:44 +0100

    Zabawy z typami danych

---
+++ TypyDanych.java
@@ -0,0 +1,27 @@
+package typy;
+import java.io.*;
+
+class TypyDanych{
+	public static void main(String [] Args)
+	{
+		String imie;
+		imie = "Katarzyna";
+		System.out.println("Dzień dobry nazywam się: "+imie);
+		
+		int wiek;
+		wiek = 20; 
+		System.out.println("W tym roku kończę: "+ wiek + "lat");
+		
+		System.out.println("Zmienna typu liczbowego int zawiera się na 4-rech bajtach "
+		 + "od -2 147 483 648 do 2 147 483 647 bitów"
+		 + " 1 Byte =  8 Bit");
+		
+		System.out.println("Zmienna typu liczbowego double zawiera się na 8-miu bajtach "
+		 + "i może posiadać 15 miejsc po przecinku od -2^63 do (2^63)-1 "
+		 + "Tj -9 223 372 036 854 775 808‬ do +9 223 372 036 854 775 807");
+		
+		
+		
+		
+	}
+}
